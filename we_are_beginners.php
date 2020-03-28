81e323993c7a6c281e1af194ae2567248bb21cfb
otengkwame <developerkwame@gmail.com>
2020-03-28 12:32:56 +0000

    we are the heart beat of php

---
+++ we_are_beginners.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Raise issues and concerns on what makes it difficult
+ * to learn and work with PHP and we will assist you
+ * from time to time
+ */
+
+echo "We are beginners, we are the heart-of-php";
