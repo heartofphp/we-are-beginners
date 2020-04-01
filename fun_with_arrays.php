018e49740ba236f973b7527cd5c63d2b1ce38451
otengkwame <developerkwame@gmail.com>
2020-04-01 14:41:21 +0000

    having fun with PHP arrays

---
+++ fun_with_arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Having some fun with arrays
+ */
+
+$resources = [
+    'frameworks' => [
+        'Laravel',
+        'Symfony',
+        'CodeIgniter'
+    ],
+    
+    'Youtube Channels' => [
+        'Traversy Media',
+        'The Net Ninja',
+        'Clever Techie',
+        'Codeholic'
+    ],
+
+    'Others' => [
+        'laracasts.com',
+        'packagist.com',
+        'phpclasses.org'
+    ],
+
+    'More to come',
+];
